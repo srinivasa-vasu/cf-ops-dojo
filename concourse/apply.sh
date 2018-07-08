3b7de2d1c84a3c22f75d5a69cf341a2e131ff16e
Srinivasa Vasu <srinivasan.surprise@gmail.com>
2018-07-08 09:26:13 +0530

    cmd update

--- concourse/apply.sh
+++ concourse/apply.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
   
-bosh -e cn-bosh deploy -d concourse concourse-deployment/cluster/concourse_uaa_credhub.yml --vars-store=creds.yml -v external_ip=<public_ip> -v db_ip=<db_ip> -l concourse-deployment/versions.yml -v internal_ip=<internal_ip>
+bosh -e cn-bosh deploy -d concourse concourse_uaa_credhub.yml --vars-store=creds.yml -v external_ip=<public_ip> -v db_ip=<db_ip> -l versions.yml -v internal_ip=<internal_ip>
