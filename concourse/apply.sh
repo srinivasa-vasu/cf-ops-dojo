32327ddd2709b928d1cf8eec9e2265142a92507f
Srinivasa Vasu <srinivasan.surprise@gmail.com>
2018-07-08 12:25:10 +0530

    add ext host variable

--- concourse/apply.sh
+++ concourse/apply.sh
@@ -2,4 +2,4 @@
   
 #bosh -e cn-bosh deploy -d concourse concourse_uaa_credhub.yml --vars-store=creds.yml -v external_ip=<public_ip> -v db_ip=<db_ip> -l versions.yml -v internal_ip=<internal_ip>
 
-bosh -e cn-bosh deploy -d concourse concourse_uaa_credhub.yml --vars-store=creds.yml -v external_ip=<public_ip> -v db_ip=<db_ip> -v external_ip=<etx_ip>
+bosh -e cn-bosh deploy -d concourse concourse_uaa_credhub.yml --vars-store=creds.yml -v external_ip=<public_ip> -v db_ip=<db_ip> -v external_ip=<ext_ip> -v external_host=<ext_host>
